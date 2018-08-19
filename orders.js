de395df4570acf2d3977582fdd3dec85258a8689
Alex4747 <rock59538@gmail.com>
2018-08-19 21:08:17 +0300

    localStorage

---
+++ orders.js
@@ -0,0 +1,51 @@
+//на document.onload загрузить все заказы из localstorage и сохранить в переменную             localstorage.getItem
+//в цыкле все заказы добавить в таблицу  создать tr  th 
+
+
+var subminButton = document.getElementById("form-registration-button-1");
+
+subminButton.onclick = function(){
+    var ordersLS = localStorage.getItem("orders");
+    var orders = [];
+    console.log(ordersLS);
+    if (ordersLS){
+        orders = JSON.parse(ordersLS);
+    }
+    orders.push(getOrder());
+    localStorage.setItem("orders", JSON.stringify(orders));
+};
+
+function getFlavores(){
+    var checkboxes = $('input.order-form-flavor:checked');
+    var flavores = [];
+      for (var i=0; i < checkboxes.length; i++){
+       flavores.push(checkboxes[i].value);
+    }
+    return flavores;
+}
+
+function getDelivery(){
+    var radio = $('input.order-form-delivery:checked');
+    return radio[0].value;
+}
+
+function getOrder(){
+    var order = {     
+        date: document.getElementById("date").value,    
+        name: document.getElementById("name").value,    
+        phone: document.getElementById("phone").value, 
+        flavores: getFlavores(),   
+        delivery: getDelivery(),   
+        comment: document.getElementById("comment").value,  
+        adress: document.getElementById("adress").value
+    };
+    return order;
+}
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -4,4 +4,51 @@ if (n === null) {
 }
 n++; 
 localStorage.setItem("on_load_counter", n);
-document.getElementById('counter').innerHTML = n;
+
+var subminButton = document.getElementById("form-registration-button-1");
+
+subminButton.onclick = function(){
+    var ordersLS = localStorage.getItem("orders");
+    var orders = [];
+    console.log(ordersLS);
+    if (ordersLS){
+        orders = JSON.parse(ordersLS);
+    }
+    orders.push(getOrder());
+    localStorage.setItem("orders", JSON.stringify(orders));
+};
+
+function getFlavores(){
+    var checkboxes = $('input.order-form-flavor:checked');
+    var flavores = [];
+      for (var i=0; i < checkboxes.length; i++){
+       flavores.push(checkboxes[i].value);
+    }
+    return flavores;
+}
+
+function getDelivery(){
+    var radio = $('input.order-form-delivery:checked');
+    return radio[0].value;
+}
+
+function getOrder(){
+    var order = {     
+        date: document.getElementById("date").value,    
+        name: document.getElementById("name").value,    
+        phone: document.getElementById("phone").value, 
+        flavores: getFlavores(),   
+        delivery: getDelivery(),   
+        comment: document.getElementById("comment").value,  
+        adress: document.getElementById("adress").value
+    };
+    return order;
+}
+
+
+
+
+
+
+
+
